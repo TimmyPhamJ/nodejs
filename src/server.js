cc7a9a9a2d4a7a45b1558663aeefaabbb959cbbf
Timmy Phạm <minhthatra@gmail.com>
2023-09-02 02:41:12 +0700

    hoidanit v21
    dư an thực tế

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import viewEngine from '../src/config/viewEngine';
 import initWebRoutes from './route/web';
+import connectDB from './config/connectDB';
+
 require('dotenv').config();
 
 let app = express();
@@ -13,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB(app);
 
 // Port === undefined => Port = 8888
 let port = process.env.PORT || 8888;
